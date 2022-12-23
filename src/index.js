3d0422dc8032fb1c4c12bba12572a3bb192a0ce1
Divyaakula28 <divya2000akula@gmail.com>
2022-12-23 13:57:14 +0530

    snd3

--- src/index.js
+++ src/index.js
@@ -10,7 +10,7 @@ import reportWebVitals from './reportWebVitals';
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
   <React.StrictMode>
-    <HashRouter>
+    <HashRouter basename='/testing1'>
      <App/>
     </HashRouter>
   </React.StrictMode>
