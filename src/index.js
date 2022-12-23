739e685adce42e8218ee24bbeb66f1a0bd604294
Divyaakula28 <divya2000akula@gmail.com>
2022-12-23 12:58:20 +0530

    trail1

--- src/index.js
+++ src/index.js
@@ -3,14 +3,16 @@ import ReactDOM from 'react-dom/client';
 import './index.css';
 import 'bootstrap/dist/css/bootstrap.min.css';
 import 'bootstrap/dist/js/bootstrap.bundle.min';
-
+import {HashRouter} from "react-router-dom";
 import App from './App';
 import reportWebVitals from './reportWebVitals';
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
   <React.StrictMode>
-    <App/>
+    <HashRouter>
+     <App/>
+    </HashRouter>
   </React.StrictMode>
 );
 
