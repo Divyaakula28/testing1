efde317bb5526c161f4413836f35aea08c55996f
Divyaakula28 <divya2000akula@gmail.com>
2022-12-23 13:07:50 +0530

    trail2

--- src/App.js
+++ src/App.js
@@ -8,17 +8,17 @@ import Content from "./Components/Content"
 import 'bootstrap/dist/css/bootstrap.min.css';
 import 'bootstrap/dist/js/bootstrap.bundle.min';
 
-import {Route, Routes} from "react-router-dom";
+import {Route, Routes,HashRouter} from "react-router-dom";
 
 function App() {
   return (
-    
+    <HashRouter>
       <Routes>
         <Route exact path='/submagic' element={<Apps/>}Route/>
         <Route   path="/magic_table" element={<Secondpage />} />
         <Route exact path='/' element={<Content/>}Route/>
-
       </Routes>
+    </HashRouter>
   );
 }
 
